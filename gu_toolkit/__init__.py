d697fa4dcf1afc55dc2e2dcd9a6394226b80f552
Gennady Uraltsev <genandy.uraltsev@gmail.com>
2026-01-21 17:05:44 -0600

    update gu_toolkit

--- gu_toolkit/__init__.py
+++
@@ -1,7 +0,0 @@
-from .prelude import *
-from .NamedFunction import NamedFunction as NamedFunction
-from .numpify import numpify as numpify, numpify_cached 
-from .SmartFigure import SmartFigure as Figure
-# from .SmartException import *
-# from .SmartFigure import *
-
